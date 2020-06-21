80bc4a821bfcc35ab9e63b455b51c2aff6e2df6c
Filip Kosmala <kosmala.filip@gmail.com>
2020-06-21 19:32:54 +0200

    Add project files.

---
+++ AnimeTracker/AnimeTracker/ViewModels/HomePageViewModel.cs
@@ -0,0 +1,12 @@
+﻿using Prism.Navigation;
+
+namespace AnimeTracker.ViewModels
+{
+    public class HomePageViewModel : ViewModelBase
+    {
+        public HomePageViewModel(INavigationService navigationService) : base(navigationService)
+        {
+            Title = "Home";
+        }
+    }
+}
---
+++ AnimeTracker/AnimeTracker/Views/RootTabbedPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace AnimeTracker.Views
+{
+    public partial class RootTabbedPage : TabbedPage
+    {
+        public RootTabbedPage()
+        {
+            InitializeComponent();
+        }
+    }
+}
